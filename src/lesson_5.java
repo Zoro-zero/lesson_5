748efeb6aa4017b09f319f2d3fe6d1b1e7fd8675
zoro <nurtilekg11@gmail.com>
2022-07-15 18:26:57 +0300

    hero

--- src/lesson_5.java
+++ src/lesson_5.java
@@ -1,27 +1,25 @@
 import java.util.Arrays;
 
 public class lesson_5 {
+
     public static void main(String[] args) {
-Boss boss=new Boss();
-boss.setBossDamage(50);
-boss.setHealth(400);
-boss.setProtection("Magical");
-        System.out.println("Damage boss: "+boss.getBossDamage()+" "+" Health boss : "+boss.getHealth()+
-                " Protection boss :"+boss.getProtection());
+        Boss boss = new Boss();
+
+        boss.setBossDamage(50);
+        boss.setHealth(400);
+        boss.setProtection("Magical");
+        System.out.println("Damage boss: " + boss.getBossDamage() + " " + " Health boss : " + boss.getHealth() +
+                " Protection boss :" + boss.getProtection());
         createHeroes();
+        for (int i = 0; i < createHeroes().length; i++) {
+            System.out.println("Damage hero : "+createHeroes()[i].getDamage() +" "+ "Helth: " +createHeroes()[i].getHealth() +" Superpower: "+ createHeroes()[i].getSuperPower());
+        }
     }
-    public static Object[] createHeroes(){
-        Hero thor=new Hero(280,33);
-        Hero artur=new Hero(300,45,"Excalibur");
-        Hero zoro=new Hero(220,22);
-        Object[] heroes=new Object[6];
-        heroes[0]=thor.getDamage();
-        heroes[1]=thor.getHealth();
-        heroes[2]=artur.getHealth();
-        heroes[3]=artur.getDamage();
-        heroes[4]=artur.getSuperPower();
-        heroes[5]=zoro.getHealth();
-        heroes[6]=zoro.getDamage();
-        return createHeroes();
+        public static  Hero[] createHeroes(){
+            Hero thor=new Hero(280,33);
+            Hero artur=new Hero(300,45,"Excalibur");
+            Hero zoro=new Hero(220,22);
+            Hero[] heroes ={thor,artur,zoro};
+            return heroes;
     }
 }
